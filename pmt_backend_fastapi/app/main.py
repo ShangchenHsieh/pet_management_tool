3a5f9b1a358e119533e8314ca9f21dfeeeaa620a
sean <sean@Siqus-MacBook-Pro-3.local>
2024-04-16 21:28:46 -0700

    Apr 16 configured on mac

--- pmt_backend_fastapi/app/main.py
+++ pmt_backend_fastapi/app/main.py
@@ -5,17 +5,17 @@ from database import engine
 import models
 
 
-# activate venv
+# activate venv on MacOS / Linux
 # source env/bin/activate
+
+# start server on MacOS: "$ python3 -m uvicorn main:app --reload" (system path issue)
+# start server on Linux: "$ uvicorn main:app --reload"
 
 models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
 app.include_router(test.router)
-
-
-
 
 
 @app.get("/")
