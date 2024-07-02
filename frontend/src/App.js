e061084e855cbb5001d72c2213262fdb878a7aa0
khmorad <khakho.morad@gmail.com>
2024-07-02 00:49:47 -0700

    implemented Navbar for frontend + reactRouting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,14 @@
-import logo from './assets/cats.jpg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Dashboard from "./components/Dashboard";
 function App() {
+  console.log(process.env.REACT_APP_API_KEY)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="dashboard" element={<Dashboard/>} />
+    </Routes>
   );
 }
 
