c82e45776bbd50849ba39cdbb1836ce32ce7ad07
Shangchen Hsieh <seankh4444@gmail.com>
2024-07-03 21:36:45 -0700

    Jul 3 nav-bar links hooked up

---
+++ frontend/src/components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Contact = () => {
+  return (
+    <div>
+        <p>
+            This is the contact page
+        </p>
+    </div>
+  );
+}
+
+export default Contact;
